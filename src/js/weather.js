ca260b8f20426ee3e64249b770e407f4dce29c90
Lucas Ferreira <ferreiralucas090@gmail.com>
2023-03-05 19:58:53 -0300

    feat: get city weather

--- src/js/weather.js
+++ src/js/weather.js
@@ -17,3 +17,20 @@ const getCityData = async cityName => {
         alert(`${name}: ${message}`)
     }
 }
+
+const getCityWeather = async cityName => {
+    try {
+        const { Key } = await getCityData(cityName)
+        const cityWeatherUrl = `http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${APIKey}`
+        const response = await fetch(cityWeatherUrl)
+
+        if(!response.ok){
+            throw new Error('Não foi possível obter os dados.')
+        }
+
+        const [cityWeatherData] = await response.json()
+        return cityWeatherData
+    } catch ({name, message}) {
+        alert(`${name}: ${message}`)
+    }
+}
