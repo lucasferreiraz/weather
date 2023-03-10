103646dcb4309b88e52f3e636dd72b8d86f696ba
Lucas Ferreira <ferreiralucas090@gmail.com>
2023-03-10 10:22:45 -0300

    refactor: refactoring functions in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -7,23 +7,30 @@ const timeIconContainer = document.querySelector('[data-js="time-icon"]')
 
 let timeImg = document.querySelector('[data-js="time"]')
 
-cityForm.addEventListener('submit', async event => {
-    event.preventDefault()
+const showCityCard = () => {
+    if (cityCard.classList.contains('d-none'))
+    cityCard.classList.remove('d-none')
+}
 
-    const inputValue = event.target.city.value
-    const [{ Key, LocalizedName }] = await getCityData(inputValue)
+const showCityWeatherInfo = async (cityName) => {
+    const [{ Key, LocalizedName }] = await getCityData(cityName)
     const [{ WeatherText, Temperature, IsDayTime, WeatherIcon }] = await getCityWeather(Key)
     const timeIcon = `<img src="./img/icons/${WeatherIcon}.svg" />`
 
+    timeImg.src = IsDayTime ? './img/day.svg' : './img/night.svg'
     timeIconContainer.innerHTML = timeIcon
     cityNameContainer.textContent = LocalizedName
     cityWeatherContainer.textContent = WeatherText
     cityTemperatureContainer.textContent = Temperature.Metric.Value
+}
 
-    if (cityCard.classList.contains('d-none'))
-        cityCard.classList.remove('d-none')
+cityForm.addEventListener('submit', event => {
+    event.preventDefault()
 
-    timeImg.src = IsDayTime ? './img/day.svg' : './img/night.svg'
+    const inputValue = event.target.city.value
+
+    showCityCard()
+    showCityWeatherInfo(inputValue)
 
     cityForm.reset()
 })
